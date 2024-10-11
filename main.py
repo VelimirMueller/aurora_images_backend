ee57f1f72526a225b5631ad03c3b840ea6b68d22
velimir <velimir.mueller@googlemail.com>
2024-10-11 22:16:45 +0200

    fix cors issues

--- main.py
+++ main.py
@@ -16,12 +16,13 @@ app = FastAPI()
 origins = [
     "http://localhost",
     "http://localhost:8000",
-    "http://localhost:5173"
+    "http://localhost:5173",
+    "http://172.17.0.3:8080",
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins="*",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
--- src/scraper.py
+++ src/scraper.py
@@ -29,18 +29,37 @@ def get_topics(url):
         topic_headline = result.find_all('span', class_="ember-view")[0].get_text()
         topic_content = result.find_all('span', class_="ember-view")[1].get_text()
         topic_link = result.find_all('a', class_="search-link")[0].get('href')
-        
+        topic_image_sources = []
+
+        print("fetching image sources from topic url: " + BASE_URL + topic_link)
+        browser.get(BASE_URL + topic_link)
+        html = browser.page_source
+        soup = BeautifulSoup(html, 'html.parser')
+
+        for image_wrapper in soup.find_all('div', class_="lightbox-wrapper"):
+            print(image_wrapper.find('img').get('src'))
+            topic_image_sources.append(image_wrapper.find('img').get('src'))
+        print()
+        print("found following relevant images: ")
+        print(topic_image_sources)
+        print()
         topic_attributes[str(id)]['topic_status'] = "Dieses Thema hat eine Lösung" in str(topic_status)
         topic_attributes[str(id)]['topic_headline'] = topic_headline.replace('\n', '').replace('  ', '')
         topic_attributes[str(id)]['topic_content'] = topic_content.replace('\n', '').replace('  ', '')
         topic_attributes[str(id)]['topic_link'] = BASE_URL + topic_link.replace('\n', '').replace('  ', '')
+        topic_attributes[str(id)]['topic_images'] = topic_image_sources
         topic_attributes[str(id)]['topic_id'] = str(id)
 
-        if topic_attributes[str(id)]['topic_status'] == True:
-            results.append(topic_attributes[str(id)])
-
+        results.append(topic_attributes[str(id)])
+        if len(results) > 5:
+            break
     browser.quit()
 
+
+    print()
+    print("printing results:")
+    print(results)
+    print()
     return results
 
 
